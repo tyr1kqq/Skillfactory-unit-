81acc4290e135760909ba11661d1f6824e007f77
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-11 16:41:14 +0200

    Добавьте файлы проекта.

---
+++ Skillfactory unit n1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+        string myName;
+        myName = "Jane";
+
+        Console.WriteLine(myName);
+
+        Console.ReadKey();
+    }
+}
