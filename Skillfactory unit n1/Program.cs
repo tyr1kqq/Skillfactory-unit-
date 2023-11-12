4c02e40b89df8a346452b7134d46e768efd28aaa
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-12 13:58:27 +0200

    Небольшое интервью

--- Skillfactory unit n1/Program.cs
+++ Skillfactory unit n1/Program.cs
@@ -5,9 +5,15 @@ class MainClass
     public static void Main(string[] args)
     {
 
-        string myName;
-        myName = "Igor";
+        string myName = "Igor";
+        byte MyAge = 23;
+        bool MyPeet = true;
+        double MyShoeSize = 41;
 
+        Console.WriteLine("My name is " + myName);
+        Console.WriteLine("My age is " + MyAge);
+        Console.WriteLine("I have pet? " + MyPeet);
+        Console.WriteLine("Shoe size - " + MyShoeSize);
         Console.WriteLine(myName);
 
         Console.ReadKey();
