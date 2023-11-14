776cd7f2a2d39e680bfface8ac7aee2a172929fb
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-14 22:33:55 +0200

    Изучения  Enum

---
+++ Skillfactory/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        Semaphore Color;
+        Color = (Semaphore)300;
+        Console.WriteLine(Color);
+    }
+}
+
+enum Semaphore : int
+{
+   red =100,
+   yellow =200,
+   green = 300,
+
+}
