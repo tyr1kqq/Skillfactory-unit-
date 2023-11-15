1521bb2657f5b15b98c76da8a85a1252fc4f0aa7
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-15 14:01:27 +0200

    Добавлено интервью с перечеслением недель, конвертация и интервью в одном коде, запускается через switch

--- Преобразования , обработка ошибок/Program.cs
+++ Преобразования , обработка ошибок/Program.cs
@@ -6,15 +6,45 @@ namespace Convet
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Say your age");
-            byte age = checked ((byte) int.Parse(Console.ReadLine()));
-            // checked проверяет правильность вывода, при сужении выдает ошибку 
-            Console.WriteLine(age);
+            Console.WriteLine("1- конвертация TryParse , checked, 2- код с перечесленим");
+            
+            string s = Console.ReadLine();
 
-            Console.WriteLine("Say age your brather");
-            byte ageb;
-            bool iscorrect =  byte.TryParse(Console.ReadLine(), out ageb);
-            Console.WriteLine("ageb = {0}",ageb);
+            switch (s)
+            {
+
+                case "1":
+
+
+                    Console.WriteLine("Say your age");
+                    byte age = checked((byte)int.Parse(Console.ReadLine()));
+                    // checked проверяет правильность вывода, при сужении выдает ошибку 
+                    Console.WriteLine(age);
+
+                    Console.WriteLine("Say age your brather");
+                    byte ageb;
+                    bool iscorrect = byte.TryParse(Console.ReadLine(), out ageb);
+                    Console.WriteLine("ageb = {0}", ageb);
+                    // конвертация метотодом tryParse , при ошибке выдает булевое значение 0
+
+                break;
+
+                    case "2":
+                    Console.Write("Enter your name:");
+                    string name = Console.ReadLine();
+                    Console.Write("Enter your age:");
+                    string ages = Console.ReadLine();
+                    Console.WriteLine("Yuor Name is {0}, your age {1}", name, ages);
+                    Console.Write("What is faworit day of week ");
+                   
+
+                    DayOfWeek day = (DayOfWeek) int.Parse( Console.ReadLine());
+
+                    Console.WriteLine("Your favirit day {0}", day);
+
+
+                    break;
+            }
         }
     }
 }
