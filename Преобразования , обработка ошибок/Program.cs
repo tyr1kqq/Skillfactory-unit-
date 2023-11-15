f7871835fc93c7677be48a3b5a0ae8f25432efc3
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-15 13:15:12 +0200

    Добавлены вариации кнвертации с описанием

--- Преобразования , обработка ошибок/Program.cs
+++ Преобразования , обработка ошибок/Program.cs
@@ -8,7 +8,13 @@ namespace Convet
         {
             Console.WriteLine("Say your age");
             byte age = checked ((byte) int.Parse(Console.ReadLine()));
+            // checked проверяет правильность вывода, при сужении выдает ошибку 
             Console.WriteLine(age);
+
+            Console.WriteLine("Say age your brather");
+            byte ageb;
+            bool iscorrect =  byte.TryParse(Console.ReadLine(), out ageb);
+            Console.WriteLine("ageb = {0}",ageb);
         }
     }
 }
