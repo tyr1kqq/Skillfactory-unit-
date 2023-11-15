8ab95a19f8df3fc5c820fdc81588b6920583e605
tyr1kqq <igorturkinwhite@yandex.ru>
2023-11-15 02:14:59 +0200

    Мат. Операции

---
+++ Skillfactory мат.операции/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lesson
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            double a, b;
+            a = 5;
+            b = 2;
+            double c = a / b;
+            Console.WriteLine(c);
+
+        }
+    }
+}
--- Skillfactory/Program.cs
+++ Skillfactory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml.Linq;
 
 class MainClass
 {
@@ -7,6 +8,12 @@ class MainClass
         Semaphore Color;
         Color = (Semaphore)300;
         Console.WriteLine(Color);
+
+        var name = "Jane";
+        var age = "27";
+        var favcolor = "black";
+
+        Console.WriteLine("{0} \n {1} \n {2}", name, age, favcolor);
     }
 }
 
